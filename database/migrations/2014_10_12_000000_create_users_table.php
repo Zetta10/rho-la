5c129113e6c929419b3ca14805b41c011ef5f2de
Zetta10 <chokinmxd18@gmail.com>
2020-05-21 19:55:54 -0700

    primer cambio

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firts_name');
-            $table->string('username');
+            $table->string('first name');
+            $table->string('last name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
